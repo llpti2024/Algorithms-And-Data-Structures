2569f7e9ffa9623ff8942cfc3e5965f288eaed2c
Lucas <llpti2024@gmail.com>
2024-12-04 14:07:37 -0300

    Updating my repositore with Algorithms and Structure Data

--- DataStructuresAndAlgorithms/src/ExecutationTimeinJava.java
+++ DataStructuresAndAlgorithms/src/ExecutationTimeinJava.java
@@ -1,5 +1,20 @@
 public class ExecutationTimeinJava {
-	public static void main ( String[] args ) {
+	public static void main ( String[] args ) throws InterruptedException {
 
+		long start = System.nanoTime ();
+		//----------program--------------
+
+		Thread.sleep ( 3000);
+
+		//-----------------------------------
+
+		long duration = (System.nanoTime ( )- start);
+
+		System.out.println("Time taken by code is : " + duration + " nanoseconds");
+		System.out.println("Time taken by code is : " + duration / 1_000_000 + " milliseconds");
+		System.out.println("Time taken by code is : " + duration / 1_000_000_000 + " seconds");
+		System.out.println("Time taken by code is : " + duration / (1_000_000_000L * 60) + " minutes");
+		System.out.println("Time taken by code is : " + duration / (1_000_000_000L * 60 * 60) + " hours");
+		System.out.println("Time taken by code is : " + duration / (1_000_000_000L * 60 * 60 * 24) + " days");
 	}
 }
